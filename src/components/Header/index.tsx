62350a3df578a10dad6881cdea0837e1eb39aef3
Everton santos <everton0102@gmail.com>
2022-03-07 16:45:36 -0300

    header estilizado e com o active no menu

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import logo from "../../../public/images/logo.png";
+import { ActiveLink } from "../ActiveLink";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <a>
+          <Image src={logo} alt="logomarca" height={90} width={90} />
+        </a>
+        <nav>
+            <ActiveLink href='/' activeClassName={styles.active}>
+              <a>Home</a>
+            </ActiveLink>
+            <ActiveLink href='/catalog' activeClassName={styles.active}>
+              <a>Catálagos</a>
+            </ActiveLink>
+            <ActiveLink href='/sobre' activeClassName={styles.active}>
+              <a>Quem somos?</a>
+            </ActiveLink>
+            </nav>
+            <a className={styles.readyButton} type='button' href="https://www.boladaomodas.com.br">Começar</a>
+
+      </div>
+    </header>
+  );
+}
