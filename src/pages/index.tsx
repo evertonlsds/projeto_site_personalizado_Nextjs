0c999f09a5c5063ba1bdd4e53ae17dcfa6ad471b
Everton santos <everton0102@gmail.com>
2022-03-07 22:41:29 -0300

    main ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             <h1>Lorem ipsum dolor sit amet dolor.</h1>
             <span>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Assumenda, sapiente! Lorem ipsum, dolor sit amet 
+              Assumenda, sapiente! Lorem ipsum, dolor sit amet
             </span>
             <a>
               <button>COMEÇAR AGORA</button>
@@ -21,6 +21,36 @@ export default function Home() {
           </section>
           <img src="/images/modeloDesenho.png" alt="modelo desenho" />
         </div>
+
+        <hr className={styles.divisor} />
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <span>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam ut aperiam cumque minus repellendus eveniet ab eum id
+              quae?
+            </span>
+          </section>
+          <img src="/images/tommy4.png" alt="marcas" />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+         <img src="/images/entrega.png" alt="entrega" />
+
+          <section>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <span>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam ut aperiam cumque minus repellendus eveniet ab eum id
+              quae?
+            </span>
+          </section>
+          
+        </div>
+
       </main>
     </>
   );
